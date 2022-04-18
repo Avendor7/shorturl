bb57ee9d25fdc6dbbb8db6b448b909af14cdb71c
Stephen <steve.wiggins7@gmail.com>
2022-04-17 23:27:18 -0400

    create new component

---
+++ src/components/ShortURLMainComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h2>ShortURL URL Shortener</h2>
+      </v-col>
+
+      
+        
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'ShortURLMainComponent',
+
+    data: () => ({
+     
+    }),
+  }
+</script>
