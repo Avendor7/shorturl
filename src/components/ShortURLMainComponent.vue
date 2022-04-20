97fa2d86719ae59b1b93228b7b9ff5b445987ac6
Stephen <steve.wiggins7@gmail.com>
2022-04-19 23:37:23 -0400

    add the form fields

--- src/components/ShortURLMainComponent.vue
+++ src/components/ShortURLMainComponent.vue
@@ -4,6 +4,14 @@
       <v-col cols="12">
         <h2>ShortURL URL Shortener</h2>
         <p>Created using Vue2, Vuetify, and the shortco.de API</p>
+        <v-text-field
+            v-model="title"
+            :rules="rules"
+            counter="25"
+            hint="This field uses counter prop"
+            label="Shorten URL"
+          ></v-text-field>
+          <v-btn elevation="2">Shorten</v-btn>
       </v-col>
 
       
@@ -14,7 +22,7 @@
 
 <script>
 
-import axios from 'axios';
+//import axios from 'axios';
 
   export default {
     name: 'ShortURLMainComponent',
@@ -23,8 +31,13 @@ import axios from 'axios';
         
     },
 
-    data: () => ({
-     
-    }),
+    data () {
+      return {
+        title: 'URL',
+        description: 'California is a state in the western United States',
+        rules: [v => v.length <= 25 || 'Max 25 characters'],
+        wordsRules: [v => v.trim().split(' ').length <= 5 || 'Max 5 words'],
+      }
+    },
   }
 </script>
