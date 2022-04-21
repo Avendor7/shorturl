a469d692d1f7e9d9afe2ae48a64d39a2c39c1262
Stephen <steve.wiggins7@gmail.com>
2022-04-20 21:17:28 -0400

    validation

--- src/components/ShortURLMainComponent.vue
+++ src/components/ShortURLMainComponent.vue
@@ -6,9 +6,9 @@
         <p>Created using Vue2, Vuetify, and the shortco.de API</p>
         <v-text-field
             v-model="title"
-            :rules="rules"
-            counter="25"
-            hint="This field uses counter prop"
+            :rules="urlRules"
+            counter="256"
+            hint="https://google.ca"
             label="Shorten URL"
           ></v-text-field>
           <v-btn elevation="2">Shorten</v-btn>
@@ -35,7 +35,11 @@
       return {
         title: 'URL',
         description: 'California is a state in the western United States',
-        rules: [v => v.length <= 25 || 'Max 25 characters'],
+        urlRules: [
+          v => !!v || 'URL is required',
+          v => (v && v.length <= 256) || 'URL must be less than 256 characters', /* eslint-disable-next-line no-useless-escape*/
+          v => /https?:[0-9]*\/\/[\w!?/\+\-_~=;\.,*&@#$%\(\)\'\[\]]+/.test(v) || 'URL must be valid',
+        ],
         wordsRules: [v => v.trim().split(' ').length <= 5 || 'Max 5 words'],
       }
     },
