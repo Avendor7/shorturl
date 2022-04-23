96207489e35be63775e2ce77f9b8bff02d1efa8d
Stephen <steve.wiggins7@gmail.com>
2022-04-22 22:49:39 -0400

    submit and use short link

--- src/components/ShortURLMainComponent.vue
+++ src/components/ShortURLMainComponent.vue
@@ -5,15 +5,15 @@
         <h2>ShortURL URL Shortener</h2>
         <p>Created using Vue2, Vuetify, and the shortco.de API</p>
         <v-text-field
-            v-model="title"
+            v-model="url"
             :rules="urlRules"
             counter="256"
             hint="https://google.ca"
             label="Shorten URL"
           ></v-text-field>
-          <v-btn elevation="2">Shorten</v-btn>
+          <v-btn elevation="2" v-on:click="getURL()">Shorten</v-btn>
       </v-col>
-
+      <h1><a v-bind:href="shortenedURL">{{shortenedURL}}</a></h1>
       
         
     </v-row>
@@ -22,7 +22,7 @@
 
 <script>
 
-//import axios from 'axios';
+import axios from 'axios';
 
   export default {
     name: 'ShortURLMainComponent',
@@ -30,10 +30,28 @@
     mounted () {
         
     },
-
+    methods: {
+      getURL(){
+        const url = "https://api.shrtco.de/v2/shorten?url="+ this.url;
+        axios.get(url)
+          .then(response => {
+              //send to the copy thingy
+              console.log(response.data.result.full_short_link);
+              this.shortenedURL = response.data.result.full_short_link
+          })
+          .catch(async function (error) {
+              const message = {
+                  ErrorMessage: error.message,
+                  Component: 'CustomerSelectorComponent',
+                  APIRoute: url
+              }
+              alert("Request Failed: " + message.ErrorMessage);
+          });
+      }
+    },
     data () {
       return {
-        title: 'URL',
+        url: 'https://google.ca',
         description: 'California is a state in the western United States',
         urlRules: [
           v => !!v || 'URL is required',
@@ -41,6 +59,7 @@
           v => /https?:[0-9]*\/\/[\w!?/\+\-_~=;\.,*&@#$%\(\)\'\[\]]+/.test(v) || 'URL must be valid',
         ],
         wordsRules: [v => v.trim().split(' ').length <= 5 || 'Max 5 words'],
+        shortenedURL:''
       }
     },
   }
